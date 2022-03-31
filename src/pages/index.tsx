cd6c2b8b4fb6148464cb083f18d8704a308e58d8
Leyton O'Day <leytonodayabc123@gmail.com>
2022-03-31 13:29:58 +0100

    Empty Next.js project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Recipe Finder Home" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
