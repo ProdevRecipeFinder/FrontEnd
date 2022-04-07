52ab3d869687790ef12dc8979dcaf3cfbfdc2633
Aaron Kyungu <aaronkyungu10@gmail.com>
2022-04-07 16:03:23 +0100

    Sign up link, image, centering

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
-import { Button, Center, Box } from "@chakra-ui/react";
+import { Button, Checkbox, Link, Center, Image, Box } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { InputField } from "../components/InputField";
-
+import NextLink from "next/link";
 const Login = () => {
   return (
     <React.Fragment>
       <Center>
-        <Box style={{ width: "30em" }}>
+        <Box style={{ width: "28em" }}>
+          <Center>
+            <Image
+              boxSize="200px"
+              src="http://www.melioramedicalgroup.co.uk/wp-content/uploads/2020/08/blank-avatar.png"
+              alt="avatar"
+              borderRadius="full"
+            />
+          </Center>
+          <Box>
+            <h1>Login</h1>
+            <p>
+              Don't have an acount?
+              <NextLink href="" passHref>
+                <Link> Sign Up</Link>
+              </NextLink>
+            </p>
+          </Box>
+          <br />
           <Formik
             initialValues={{ username: "", password: "" }}
             onSubmit={() => {
@@ -19,7 +37,15 @@ const Login = () => {
               <br />
               <InputField name="password" label="Password" />
               <br />
-              <Button type="submit">Login</Button>
+              <Checkbox name="rememberMe" defaultChecked>
+                Remember Me
+              </Checkbox>
+              <br />
+              <Center>
+                <Button type="submit" colorScheme="red">
+                  Login
+                </Button>
+              </Center>
             </Form>
           </Formik>
         </Box>
