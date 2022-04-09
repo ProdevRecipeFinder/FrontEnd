5481df63586693e1d228ffb200c9d6049a2f3599
Leyton O'Day <leytonodayabc123@gmail.com>
2022-04-09 15:28:41 +0100

    Added front-end for changing username

--- src/pages/settings/profile.tsx
+++ src/pages/settings/profile.tsx
@@ -1,36 +1,66 @@
-import { 
-  Box, 
-  Divider, 
-  Button } 
-from "@chakra-ui/react"
+import {
+  Box,
+  Divider,
+  Button
+}
+  from "@chakra-ui/react"
 import { Form, Formik } from 'formik'
-import { InputField }   from '../../components/InputField'
-import SettingsLayout   from "../../components/layouts/SettingsLayout"
-import styles           from "../../styles/settings.module.css"
-import React            from "react"
+import { InputField } from '../../components/InputField'
+import SettingsLayout from "../../components/layouts/SettingsLayout"
+import styles from "../../styles/settings.module.css"
+import React from "react"
+
+import { useChangeUsernameMutation, WhoAmIQuery, WhoAmIDocument } from "../../generated/graphql"
+import { convertErrorMsg } from "../../utils/convertErrorMsg";
 
 const profile = () => {
+
+  const [changeUsername] = useChangeUsernameMutation()
+
   return (
     <React.Fragment>
       <Box className={styles.container}>
-      <h1 className="title">Public Profile</h1>
+        <h1 className="title">Public Profile</h1>
 
-      <Divider />
-      <br />
+        <Divider />
+        <br />
 
-      <Formik 
-        initialValues={{username: ""}}
-        onSubmit={(values, {setErrors}) => {}
-      }>
-        {
-          ({isSubmitting}) => (
-            <Form style={{width: "25em"}}>
-              <InputField name="username" label="Username" style={{width: "75%", float: "left"}} />
-              <Button type="submit" isLoading={isSubmitting} w="20%" float="right">Save</Button>
-            </Form>
-          )
-        }
-      </Formik>
+        <Formik
+          initialValues={{ username: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await changeUsername({
+              variables: values,
+              update: (caches, { data }) => { // Updating the cache for live reload
+                caches.writeQuery<WhoAmIQuery>({
+                  query: WhoAmIDocument,
+                  data: {
+                    __typename: "Query",
+                    whoami: data?.changeUsername.user,
+                  }
+                })
+              }
+            })
+
+            if (response.data?.changeUsername.errors) {
+              //handle errors
+              setErrors(convertErrorMsg(response.data.changeUsername.errors));
+            }
+            else if (response.data?.changeUsername.user) {
+              //handle success
+              //send notification saying the user logged in 
+              
+            }
+          }}
+        >
+          {
+            ({ isSubmitting }) => (
+              <Form style={{ width: "25em" }}>
+                <InputField name="username" label="Username" style={{ width: "75%", float: "left" }} />
+                <Button type="submit" isLoading={isSubmitting} w="20%" float="right">Save</Button>
+              </Form>
+            )
+          }
+        </Formik>
       </Box>
     </React.Fragment>
   )
