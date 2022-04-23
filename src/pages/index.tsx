0a3fc95db68db4cf6fa4c217a8f024c92ade03fa
Leyton O'Day <leytonodayabc123@gmail.com>
2022-04-23 01:26:57 +0100

    Navbar works with mobile now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ const Home: NextPage = () => {
 
   return (
     <React.Fragment>
+      
     </React.Fragment>
   )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -5,6 +5,17 @@ const config: ThemeConfig = {
   useSystemColorMode: false,
 }
 
-const theme = extendTheme({ config })
+const breakpoints = {
+  sm: '30em',
+  md: '48em',
+  lg: '62em',
+  xl: '80em',
+  '2xl': '96em',
+}
+
+const theme = extendTheme({ 
+  config,
+  breakpoints 
+})
 
 export default theme
