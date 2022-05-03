b82a646d07415c78751850a0d4a35c85358975ed
GergoN02 <nemethgergo02@gmail.com>
2022-05-03 15:46:08 +0100

    initial imageUpload setup

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -1,97 +1,70 @@
-import { Box, Image } from '@chakra-ui/react'
-import { url } from 'inspector';
-import { useEffect, useState } from 'react';
-import Dropzone, {
-  defaultClassNames,
-  IDropzoneProps,
-  ILayoutProps,
-  IPreviewProps,
-  IInputProps,
-} from 'react-dropzone-uploader'
-import 'react-dropzone-uploader/dist/styles.css'
 
-export type ImageUploadType = {
-  passImageProps: any;
-}
+import { Dropzone, FileItem, FullScreenPreview } from "@dropzone-ui/react";
+import { useState } from "react";
 
-const ImageUpload = ({ passImageProps }: ImageUploadType) => {
+export const ImageUpload = () => {
 
-  const [imageState, setImageState] = useState({ imageState: "", uploadState: "none" });
+  const [files, setFiles] = useState([]);
+  const [imageSrc, setImageSrc] = useState(undefined);
 
-  const getUploadParams: IDropzoneProps['getUploadParams'] = ({ meta }) => {
-    const url = 'http://localhost:4000/upload-image'
-    const fileUrl = `${url}/${encodeURIComponent(meta.name)}`
-    return { url, meta: { fileUrl } }
-  }
-
-  const handleChangeStatus: IDropzoneProps['onChangeStatus'] = ({ meta }, status,) => {
-    setImageState({
-      imageState: meta.previewUrl!,
-      uploadState: status
-    })
-    console.log("this happened");
-    if (status === "done") {
-      console.log(meta);
+  const updateFiles = (incommingFiles: any) => {
+    console.log("incomming files", incommingFiles);
+    setFiles(incommingFiles);
+  };
+  const onDelete = (id: number) => {
+    if (files[0]) {
+      setFiles(files.filter((x) => x.id !== id));
     }
-    passImageProps(meta.name)
-
-  }
-
-  const handleSubmit: IDropzoneProps['onSubmit'] = (files) => {
-    console.log(files.map(f => f.meta))
-    console.log("OnSubmit called");
-
-    setImageState({
-      ...imageState,
-      uploadState: "uploaded"
-    })
-
-    console.log(files)
-  }
-
-  const Preview = ({ meta }: IPreviewProps) => {
-    const { name, percent, status, previewUrl } = meta
-    if (status !== 'done') {
-      return (
-        <Box>
-          <span style={{ alignSelf: 'flex-start', margin: '10px 3%' }}>
-            {name}, {Math.round(percent)}%, {status}
-          </span>
-        </Box>
-      )
-    }
-    else {
-      return (
-        <Box>
-          <Image
-            src={previewUrl}
-            width={'300px'}
-            height={'300px'}
-          />
-        </Box>
-      )
-    }
-  }
+  };
+  const handleSee = (imageSource: any) => {
+    setImageSrc(imageSource);
+  };
+  const handleClean = (files: any) => {
+    console.log("list cleaned", files);
+  };
 
   return (
     <Dropzone
-      accept='image/jpg,image/png,image/jpeg'
-      multiple={false}
+      style={{ minWidth: "550px" }}
+      //view={"list"}
+      onChange={updateFiles}
+      minHeight="195px"
+      onClean={handleClean}
+      value={files}
       maxFiles={1}
-      getUploadParams={getUploadParams}
-      onChangeStatus={handleChangeStatus}
-      onSubmit={handleSubmit}
-      autoUpload={true}
-      PreviewComponent={Preview}
-      disabled={files => files.some(f => ['preparing', 'getting_upload_params', 'uploading'].includes(f.meta.status))}
-      styles={{
-        dropzone: { width: 450, height: 450, overflow: 'hidden' },
-        dropzoneActive: { borderColor: 'green' },
-        submitButton: { display: "none" }
-      }}
+      //header={false}
+      // footer={false}
+      maxFileSize={2998000}
+      //label="Drag'n drop files here or click to browse"
+      //label="Suleta tus archivos aquí"
+      accept=".png,image/*"
+      // uploadingMessage={"Uploading..."}
+      url="https://my-awsome-server/upload-my-file"
+      //of course this url doens´t work, is only to make upload button visible
+      //uploadOnDrop
+      //clickable={false}
+      fakeUploading
+      //localization={"FR-fr"}
+      disableScroll
     >
+      {files.map((file) => (
+        <FileItem
+          {...file}
+          key={file.id}
+          onDelete={onDelete}
+          onSee={handleSee}
+          //localization={"ES-es"}
+          resultOnTooltip
+          preview
+          info
+          hd
+        />
+      ))}
+      <FullScreenPreview
+        imgSource={imageSrc}
+        openImage={imageSrc}
+        onClose={(e) => handleSee(undefined)}
+      />
     </Dropzone>
-  )
-}
-
-export default ImageUpload;
+  );
+};
